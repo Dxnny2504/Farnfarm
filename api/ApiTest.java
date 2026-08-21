e62a62e61ec7839cc3e6b8059a47e9092e7a216c
Dxnny2504 <r.guse858@gmail.com>
2026-08-21 23:00:08 +0200

    Update ApiTest.java

--- api/ApiTest.java
+++ api/ApiTest.java
@@ -1,39 +1,14 @@
-package org.example.core.listener;
+package org.example.api;
 
 import net.labymod.api.client.key.Key;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.input.KeyEvent;
-import org.example.core.ExampleAddon;
 
-public class FarnFarmKeyListener {
-
-  private final ExampleAddon addon;
-  private boolean active = false;
-
-  public FarnFarmKeyListener(ExampleAddon addon) {
-    this.addon = addon;
-  }
+public class ApiTest {
 
   @Subscribe
   public void onKey(KeyEvent event) {
-    if (event.key() != Key.Semicolon) {
-      return;
-    }
-
-    if (event.action().toString().equalsIgnoreCase("PRESS")) {
-      this.toggle();
-    }
-  }
-
-  public boolean isActive() {
-    return this.active;
-  }
-
-  public void toggle() {
-    this.active = !this.active;
-
-    this.addon.logger().info(
-        this.active ? "FarnFarm aktiviert" : "FarnFarm deaktiviert"
-    );
+    Key key = event.key();
+    var action = event.action();
   }
 }
