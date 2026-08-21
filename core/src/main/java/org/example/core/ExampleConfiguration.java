a0b0eedd86c118f86f8af45fe41c1ba399bd091b
Dxnny2504 <r.guse858@gmail.com>
2026-08-21 20:29:46 +0200

    Update ExampleConfiguration.java

--- core/src/main/java/org/example/core/ExampleConfiguration.java
+++ core/src/main/java/org/example/core/ExampleConfiguration.java
@@ -1,11 +1,7 @@
 package org.example.core;
 
 import net.labymod.api.addon.AddonConfig;
-import net.labymod.api.client.key.Key;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
-import net.labymod.api.configuration.settings.annotation.Setting;
-import net.labymod.api.configuration.settings.annotation.SettingSection;
-import net.labymod.api.configuration.settings.annotation.types.KeyBindSetting;
 import net.labymod.api.configuration.settings.annotation.types.SwitchSetting;
 
 public class ExampleConfiguration extends AddonConfig {
@@ -13,16 +9,8 @@ public class ExampleConfiguration extends AddonConfig {
   @SwitchSetting
   private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
-  @SettingSection("farnfarm")
-  @KeyBindSetting
-  private final ConfigProperty<Key> toggleKey = new ConfigProperty<>(Key.Semicolon);
-
   @Override
   public ConfigProperty<Boolean> enabled() {
     return this.enabled;
-  }
-
-  public ConfigProperty<Key> toggleKey() {
-    return this.toggleKey;
   }
 }
