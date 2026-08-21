465da663a7ba434b8fc0a94916abd579662e9831
Dxnny2504 <r.guse858@gmail.com>
2026-08-21 21:47:25 +0200

    Update ExampleConfiguration.java

--- core/src/main/java/org/example/core/ExampleConfiguration.java
+++ core/src/main/java/org/example/core/ExampleConfiguration.java
@@ -1,25 +1,18 @@
 package org.example.core;
 
 import net.labymod.api.addon.AddonConfig;
-import net.labymod.api.client.key.Key;
+import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
-import net.labymod.api.configuration.settings.annotation.types.KeyBindSetting;
 
 @ConfigName("settings")
 public class ExampleConfiguration extends AddonConfig {
 
+  @SwitchSetting
   private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
-
-  @KeyBindSetting
-  private final ConfigProperty<Key> toggleKey = new ConfigProperty<>(Key.Semicolon);
 
   @Override
   public ConfigProperty<Boolean> enabled() {
     return this.enabled;
-  }
-
-  public ConfigProperty<Key> toggleKey() {
-    return this.toggleKey;
   }
 }
