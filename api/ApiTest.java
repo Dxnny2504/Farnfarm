3362332af94c0df42014086caae85306052be0bc
Dxnny2504 <r.guse858@gmail.com>
2026-08-21 21:57:54 +0200

    Update ApiTest.java

--- api/ApiTest.java
+++ api/ApiTest.java
@@ -1,10 +1,13 @@
 package org.example.api;
 
 import net.labymod.api.client.key.Key;
+import net.labymod.api.event.Subscribe;
+import net.labymod.api.event.client.input.KeyEvent;
 
 public class ApiTest {
 
-  public static Key testKey() {
-    return Key.Semicolon;
+  @Subscribe
+  public void onKey(KeyEvent event) {
+    Key key = event.key();
   }
 }
