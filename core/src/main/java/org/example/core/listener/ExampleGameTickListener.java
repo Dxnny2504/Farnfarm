21c413005276364d921876468a3aa1df3ab18870
Dxnny2504 <r.guse858@gmail.com>
2026-08-21 20:15:32 +0200

    Update ExampleGameTickListener.java

--- core/src/main/java/org/example/core/listener/ExampleGameTickListener.java
+++ core/src/main/java/org/example/core/listener/ExampleGameTickListener.java
@@ -18,7 +18,5 @@ public class ExampleGameTickListener {
     if (event.phase() != Phase.PRE) {
       return;
     }
-
-    this.addon.logger().info(this.addon.configuration().enabled().get() ? "enabled" : "disabled");
   }
 }
