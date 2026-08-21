20cb2610c11412bd778baef3923accfeccf19bfd
Dxnny2504 <r.guse858@gmail.com>
2026-08-21 21:14:17 +0200

    Update FarnFarmKeyListener.java

--- core/src/main/java/org/example/core/listener/FarnFarmKeyListener.java
+++ core/src/main/java/org/example/core/listener/FarnFarmKeyListener.java
@@ -1,1 +1,39 @@
+package org.example.core.listener;
 
+import net.labymod.api.event.Phase;
+import net.labymod.api.event.Subscribe;
+import net.labymod.api.event.client.lifecycle.GameTickEvent;
+import org.example.core.ExampleAddon;
+
+public class FarnFarmKeyListener {
+
+  private final ExampleAddon addon;
+  private boolean active = false;
+
+  public FarnFarmKeyListener(ExampleAddon addon) {
+    this.addon = addon;
+  }
+
+  @Subscribe
+  public void onGameTick(GameTickEvent event) {
+    if (event.phase() != Phase.PRE) {
+      return;
+    }
+
+    if (!this.active) {
+      return;
+    }
+  }
+
+  public boolean isActive() {
+    return this.active;
+  }
+
+  public void toggle() {
+    this.active = !this.active;
+
+    this.addon.logger().info(
+        this.active ? "FarnFarm aktiviert" : "FarnFarm deaktiviert"
+    );
+  }
+}
